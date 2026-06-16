4bba6a924726c4a9fa5c5ce9494d2a865e1361ef
kavitabhutada0907-bit <kavita.bhutada0907@gmail.com>
2026-06-17 01:57:58 +0530

    Update wedding_invitation_2.tsx

--- wedding_invitation_2.tsx
+++ wedding_invitation_2.tsx
@@ -386,7 +386,7 @@ text-shadow: 0 2px 8px rgba(0,0,0,0.8);
 }
 .scroll-down-btn {
   position: absolute;
-  bottom: 130px;
+  bottom: 160px;
   left: 50%;
   transform: translateX(-50%);
   z-index: 4;
